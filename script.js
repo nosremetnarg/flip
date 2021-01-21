1d1cb29fb9e66fe9c6b385cabb76305bca525a3c
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-21 11:21:43 -0600

    other cards inserted

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const card = document.querySelector(".card__inner");
+const image = document.querySelector('.card__face--back');
+const tarot = document.querySelector('.hermit');
 
 // Titles of Tarot Cards
 const photoArray = [
-"Judgement",
-"Hermit",
-"WheelOfFortune",
-"Journey"
+
+"theHermit",
+"wheelOfFortune",
+"theJourney",
+"STRENGTH",
+"TEMPERANCE",
+"theLovers"
 
 ]
 
-// // Picks random photo index
-// const randomNumber = Math.floor(Math.random() * photoArray.length) + 1;
-// console.log(randomNumber);
+// Picks random photo index
+const randomNumber = Math.floor(Math.random() * photoArray.length);
+console.log(randomNumber);
 
-// // random photo index number
-// let photoIndex = randomNumber;
+// random photo index number
+let photoIndex = randomNumber;
 
-// // Initially load photo details into DOM
-// loadPicture(photoArray[photoIndex]);
+// Initially load photo details into DOM
+loadPicture(photoArray[photoIndex]);
 
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped')
 })
+
+function loadPicture(photo) {
+    console.log(`title ${photo}`);
+    tarot.src = `img/${photo}.png`;
+
+}
