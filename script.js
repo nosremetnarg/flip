15238f6f488d7869975e5cbb0310ff9c2b7bfd9e
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-21 22:10:47 -0600

    card title appearing now

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const card = document.querySelector(".card__inner");
 const image = document.querySelector(".card__face--back");
 const tarot = document.querySelector(".hermit");
+const cardTitle = document.querySelector('.card__text--title')
 
 // Titles of Tarot Cards
 const photoArray = [
-  "theHermit",
-  "wheelOfFortune",
-  "theJourney",
+  "the Hermit",
+  "wheel Of Fortune",
+  "the Journey",
   "strength",
   "TEMPERANCE",
-  "theLovers",
-  "theEmpress",
-  "theStar",
+  "the Lovers",
+  "the Empress",
+  "the Star",
   "judgement",
-  "theSun",
-  "theMagician",
+  "the Sun",
+  "the Magician",
 ];
-
-
-
 
 let isFlipped = false;
 let photoIndex;
@@ -29,30 +27,37 @@ function randomNumber() {
   console.log(randomNum);
   photoIndex = randomNum;
 }
-// call on page load
+// Load picture on initial page load
 randomNumber();
-loadPicture(photoArray[photoIndex])
+loadPicture(photoArray[photoIndex]);
+getPhototitle(photoArray[photoIndex])
 
 function loadPicture(photo) {
   console.log(`title ${photo}`);
-    tarot.src = `img/${photo}-min.png`;
-  
+  tarot.src = `img/${photo}-min.png`;
+}
+
+function getPhototitle(photo) {
+  let title = photo;
+  console.log(title);
 }
 
 // Event listener for clicks
 card.addEventListener("click", function () {
   card.classList.toggle("is-flipped");
   if (!isFlipped) {
-    
     isFlipped = true;
+    cardTitle.innerHTML = photoArray[photoIndex];
   } else if (isFlipped) {
     randomNumber();
-    setTimeout(function(){ loadPicture(photoArray[photoIndex]); }, 250);
+    setTimeout(function () {
+      loadPicture(photoArray[photoIndex]);
+    }, 250);
+    cardTitle.innerHTML = "&nbsp";
     isFlipped = false;
   }
   console.log(`is the card flipped ${isFlipped}`);
 });
-
 
 // window.addEventListener('load', function(e) {
 //     randomNumber();
