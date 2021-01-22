0ced8c2e6dc588f7154de9e91d42dffe7449f069
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-21 20:25:15 -0600

    minified cards

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ const photoArray = [
   "theHermit",
   "wheelOfFortune",
   "theJourney",
-  "STRENGTH",
+  "strength",
   "TEMPERANCE",
   "theLovers",
+  "theEmpress",
+  "theStar",
+  "judgement",
+  "theSun",
+  "theMagician"
 ];
-let isFlipped = false;
 
+let isFlipped = false;
 let photoIndex;
+
 // Picks random photo index
 function randomNumber() {
   let randomNum = Math.floor(Math.random() * photoArray.length);
@@ -23,31 +29,38 @@ function randomNumber() {
 function loadPicture(photo) {
     console.log(`title ${photo}`);
     if (!isFlipped) {
-      tarot.src = `img/${photo}.png`;
+      tarot.src = `img/${photo}-min.png`;
     } 
-  
   }
 
 // Event listener for clicks
 card.addEventListener("click", function () {
-    randomNumber();
-    loadPicture(photoArray[photoIndex]);
-    card.classList.toggle("is-flipped");
-  
-  if (!isFlipped) {
-    isFlipped = true;
-    // randomNumber();
+    
     // loadPicture(photoArray[photoIndex]);
-  } else if (isFlipped){
+    card.classList.toggle("is-flipped");
+    
+  if (!isFlipped) {
+    loadPicture(photoArray[photoIndex]);
+    isFlipped = true;
+
+  } else if (isFlipped) { 
+    randomNumber();
       isFlipped = false;
   }
   console.log(`is the card flipped ${isFlipped}`);
 });
 
-// function loadPicture(photo) {
-//   console.log(`title ${photo}`);
-//   if (!isFlipped) {
-//     tarot.src = `img/${photo}.png`;
-//   } 
+randomNumber();
 
-// }
+
+// Trying to do this with an object
+
+const tarotObjectArray = [
+
+    {
+        'title': 'theHermit',
+        'info': 'The lesson and reward but also misfortune of solitude.',
+        "index" : 1
+    },
+
+]
