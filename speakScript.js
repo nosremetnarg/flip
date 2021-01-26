b897ba36785483a780ea8fd2431f89f2171c280b
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-26 10:55:52 -0600

    added new passphrase

--- speakScript.js
+++ speakScript.js
@@ -25,28 +25,37 @@ function checkNumber(msg) {
     const num = +msg;
 
     // Check if valid number
-    if(Number.isNaN(num)) {
-        msgEl.innerHTML += "<div>That is not a valid number</div>";
-        return;
-    }
+    // if(Number.isNaN(num)) {
+    //     msgEl.innerHTML += "<div>That is not a valid number</div>";
+    //     return;
+    // }
 
-    // Check in range
-    if(num > 100 || num < 1) {
-        msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
-        return;
-    }
+    // // Check in range
+    // if(num > 100 || num < 1) {
+    //     msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    //     return;
+    // }
 
     // Check number
-    if(num === randomNum) {
+    // if(num === randomNum) {
+    //     document.body.innerHTML = `
+    //     <h2>Congrats! You have guessed the number! <br><br>
+    //     It was ${num}</h2>
+    //     <button class="play-again" id="play-again">Play Again</button>`
+    // } else if (num > randomNum){
+    //     msgEl.innerHTML += '<div>Go lower</div>';
+    // } else {
+    //     msgEl.innerHTML += '<div>Go higher</div>';
+    // }
+
+    // Check Message 
+    if (msg === "I am not afraid of the dark") {
         document.body.innerHTML = `
-        <h2>Congrats! You have guessed the number! <br><br>
-        It was ${num}</h2>
-        <button class="play-again" id="play-again">Play Again</button>`
-    } else if (num > randomNum){
-        msgEl.innerHTML += '<div>Go lower</div>';
+     <h2>Congrats! You have unlocked the door! <br><br>
+        `
     } else {
-        msgEl.innerHTML += '<div>Go higher</div>';
-    }
+        msgEl.innerHTML += '<div>Try again</div>';
+        }
 }
 
 // write what user speaks
