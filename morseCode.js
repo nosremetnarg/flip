552e4555dfaa46d83e589c9491534133667f2f24
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-27 20:21:33 -0600

    dark/light mode working

--- morseCode.js
+++ morseCode.js
@@ -1,15 +1,67 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.querySelector("#toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
 
+const DARK_THEME = 'dark';
+const LIGHT_THEME = 'light';
+
+// Dark or Light Images
+function imageMode(color) {
+  image1.src = `img/undraw_winter_magic_5xu2_${color}.svg`;
+  image2.src = `img/undraw_Jogging_t14q_${color}.svg`;
+  image3.src = `img/undraw_witch_7uk7_${color}.svg`;
+}
+function toggleDarkLightMode(DARK_THEME) {
+  nav.style.backgroundColor = DARK_THEME ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = DARK_THEME ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = DARK_THEME ? "Dark Mode" :"Light Mode";
+  DARK_THEME ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon") : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  DARK_THEME ? imageMode("dark") : imageMode("light");
+}
+// // Dark Mode Style
+// function darkMode() {
+//   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+//   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+//   toggleIcon.children[0].textContent = "Dark Mode";
+//   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+//   imageMode("dark");
+// }
+
+// // Light Mode Style
+// function lightMode() {
+//   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+//   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+//   toggleIcon.children[0].textContent = "Light Mode";
+//   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+//   imageMode("light");
+// }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    toggleDarkLightMode(DARK_THEME);
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    toggleDarkLightMode(LIGHT_THEME);
+  }
 }
 
-
 // Event Listener
-toggleSwitch.addEventListener('change', switchTheme);
+toggleSwitch.addEventListener("change", switchTheme);
+
+// check localStorage for theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
